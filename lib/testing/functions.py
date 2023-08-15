6ad28778730196a3f8665cb3076fe3d3cb9f6d27
+ <+>
2023-08-15 13:16:09 +0300

    done

---
+++ lib/testing/functions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+def greet_programmer():
+    print ('Hello, programmer!')
+greet_programmer()
+
+def greet(name):
+    print (f'Hello, {name}!')
+greet('Guido')
+
+def greet_with_default(name="programmer"):
+    print (f'Hello, {name}!')
+greet_with_default()
+
+def add(num1, num2):
+    return (num1 + num2)
+add(45, 55)
+
+def halve(number):
+    return (number/2)
+halve(100)
